aa0894d7373d524ec240e582a8b8c8cf5333bf00
yogaananth12-crypto <yogaananth12@gmail.com>
2025-12-26 15:29:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Spare Parts Inventory", layout="wide")
 
 SHEET_ID = "1PY9T5x0sqaDnHTZ5RoDx3LYGBu8bqOT7j4itdlC9yuE"
 CSV_URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=csv"
-SAVE_URL = "https://script.google.com/macros/s/AKfycbx0WFr35KlCjlSgCwOJB0waE86knqMt__xDy1bNKolTVdxve6LV4bwR-E9PJe13K8u8Gw/exec"
+SAVE_URL = "https://script.google.com/macros/s/AKfycbxSSOzSAl0a0NZFaRx4q9MlfGcQXsZ0hWprYNbVXH811BgGfdEuAf-V-zu1kazuGIpV8Q/exec"
 
 @st.cache_data(ttl=3)
 def load_data():
