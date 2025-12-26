921965f830ff5ef4c7dc42f571e0903f2151a005
yogaananth12-crypto <yogaananth12@gmail.com>
2025-12-26 17:42:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 
 # ================= CONFIG =================
 SHEET_ID = "1PY9T5x0sqaDnHTZ5RoDx3LYGBu8bqOT7j4itdlC9yuE"
-SAVE_URL = "https://script.google.com/macros/s/AKfycbzzVQjdwI0zdW28OyRsNt9DEZwG510uKVMQacbYZ-nXdwFqGDdmorqaVwVl04QDJgapKQ/exec"
+SAVE_URL = "https://script.google.com/macros/s/AKfycbxzy_K648Yu84hoONSSjPOj7YNdhCCZxnx0oyoQh77xA8O5UH3YkuM1l6jLLgu0XOxN0A/exec"
 CSV_URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=csv"
 
 st.set_page_config(layout="wide")
