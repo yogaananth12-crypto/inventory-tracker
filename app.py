764366b1c45f947c68aa726a474acab8361eb165
yogaananth12-crypto <yogaananth12@gmail.com>
2025-12-29 09:22:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
 
-st.title("Google Sheet Write Test")
+st.title("WRITE TEST")
 
 SHEET_ID = "1PY9T5x0sqaDnHTZ5RoDx3LYGBu8bqOT7j4itdlC9yuE"
 SHEET_NAME = "Sheet1"
@@ -20,9 +20,10 @@ creds = Credentials.from_service_account_info(
 client = gspread.authorize(creds)
 sheet = client.open_by_key(SHEET_ID).worksheet(SHEET_NAME)
 
-if st.button("WRITE TEST"):
-    sheet.update("A1", "STREAMLIT WRITE OK")
-    st.success("Sheet updated")
+if st.button("TEST WRITE"):
+    sheet.update("A1", "STREAMLIT WRITE SUCCESS")
+    st.success("Updated A1")
+
 
 
 
